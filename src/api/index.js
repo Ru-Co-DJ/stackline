01c6ab3e5791d8d69317bfacf6b25127efed0b77
Ru <99769987+Ru-Co-DJ@users.noreply.github.com>
2022-10-20 00:59:27 +0100

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,8 @@ import axios from "axios"
 
 const baseUrl = "http://localhost:5000";
 
+
+// please note that the following functions are not completely implemented, it is just the structure
 export const getSales = async () => {
     try {
         const { data } = await axios.get(`${baseUrl}/sales/all`)
